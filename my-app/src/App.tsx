1c38dc38bff77420b2a93c80fb62355b067511dc
Alex Li <alexzli@mit.edu>
2023-04-07 18:00:47 -0400

    add routing to planner page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,93 +1,16 @@
 import "./App.css";
-import { LoadScript } from "@react-google-maps/api";
-import SearchBar from "./components/SearchBar";
-import Map from "./components/Map";
-
-const API_KEY = "AIzaSyAyk1O9rcmRWSSKHN0S5Vo2_iBavhj3DtA";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Landing from "./Landing";
+import Planner from "./Planner";
 
 function App() {
   return (
-    <div
-      style={{
-        margin: "50px",
-      }}
-    >
-      <div>
-        <h2
-          style={{
-            fontWeight: 100,
-            fontSize: "24px",
-          }}
-        >
-          From:
-        </h2>
-        <LoadScript googleMapsApiKey={API_KEY} libraries={["places"]}>
-          <SearchBar apiKey={API_KEY} placeholder={"Boston, MA"} />
-        </LoadScript>
-      </div>
-      <br />
-      <div>
-        <h2
-          style={{
-            fontWeight: 100,
-            fontSize: "24px",
-          }}
-        >
-          To:
-        </h2>
-        <LoadScript googleMapsApiKey={API_KEY} libraries={["places"]}>
-          <SearchBar apiKey={API_KEY} placeholder={"New York, NY"} />
-        </LoadScript>
-      </div>
-      <br />
-      <br />
-      <div>
-        <h2
-          style={{
-            fontWeight: 100,
-            fontFamily: "Inter, sans-serif",
-            fontSize: "28px",
-            display: "inline-block",
-          }}
-        >
-          Take me somewhere
-        </h2>
-        <input
-          type="text"
-          placeholder={"beautiful"}
-          style={{
-            backgroundColor: "#FEF9EF",
-            border: "1px solid transparent",
-            borderBottom: "1px solid black",
-            fontWeight: 900,
-            width: "120px",
-            resize: "horizontal",
-            height: "35px",
-            fontSize: "28px",
-            outline: "none",
-            textOverflow: "ellipses",
-            display: "inline-block",
-            marginLeft: "6px",
-            marginRight: "12px",
-          }}
-        />
-        <button
-          style={{
-            fontWeight: 900,
-            backgroundColor: "#17C3B2",
-            border: "none",
-            color: "#FEF9EF",
-            padding: "8px 16px",
-            textAlign: "center",
-            fontSize: "28px",
-            borderRadius: "12px",
-            display: "inline-block",
-          }}
-        >
-          Go
-        </button>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/planner" element={<Planner />} />
+      </Routes>
+    </Router>
   );
 }
 
