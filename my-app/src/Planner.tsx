d38823cc7e763261f5ac64222bb924f1d29fd3df
Andy Jiang <andyjiang2016@gmail.com>
2023-04-07 18:42:06 -0400

    Creating sidebar

--- my-app/src/Planner.tsx
+++ my-app/src/Planner.tsx
@@ -1,3 +1,11 @@
-export default function Landing() {
-  return <div></div>;
+import SideBar from "./components/SideBar";
+import Map from "./components/Map";
+import "./Planner.css";
+
+export default function Planner() {
+  return (
+    <div className="PlannerContainer">
+      <SideBar />
+    </div>
+  );
 }
